7b904b5c8bfb72df049f49d935a44aacef372981
Juan C <juan.cisilino@gmail.com>
2019-08-25 14:06:53 -0300

    Ejercicio 3

---
+++ UML a JAVA/Ejercicio3/src/com/company/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+
+        ArrayList<Integer> listaNueva = new ArrayList<>();
+
+            listaNueva.add(1);
+            listaNueva.add(5);
+            listaNueva.add(5);
+            listaNueva.add(6);
+            listaNueva.add(7);
+            listaNueva.add(8);
+            listaNueva.add(8);
+            listaNueva.add(8);
+
+        System.out.println(listaNueva);
+
+
+
+    }
+}
