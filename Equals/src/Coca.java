1bbd3fe1c0fee379a77519fae3ae8438292ebeb6
Juan C <juan.cisilino@gmail.com>
2019-08-28 20:07:06 -0300

    Ejercicio Equals Coca

--- Equals/src/Coca.java
+++ Equals/src/Coca.java
@@ -1,12 +1,23 @@
 public class Coca {
 
-    private Integer tamaño;
+    private Integer tamano;
     private Double precio;
 
-    public Coca(Integer tamaño, Double precio){
+    public Coca(Integer tamano, Double precio){
         this.precio = precio;
-        this.tamaño = tamaño;
+        this.tamano = tamano;
     }
+
+    public boolean equals(Object object) {
+        Coca unaCoca = (Coca) object;
+        if(this.tamano.equals(unaCoca.tamano)&& this.precio.equals(unaCoca.precio)){
+            System.out.println(true);
+        } else {
+            System.out.println(false);
+        }
+        return false;
+    }
+
 
 
 
--- Equals/src/Main.java
+++ Equals/src/Main.java
@@ -3,6 +3,14 @@ public class Main {
     public static void main(String[] args) {
 	// write your code
 
+        Coca cocauno = new Coca( 23,76.00) ;
+        Coca cocados = new Coca( 23, 76.00);
+
+
+
+        cocauno.equals(cocados);
+
+
 
 
     }
